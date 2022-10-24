06789acfe079dcc157ae103a7f89955b46934069
Ignacio Javier Julio Posada <ignacio9876.ijjp@gmail.com>
2022-10-24 13:31:34 -0500

    fix: scroll en el menu - feat: animación al menu y el footer

--- js/home.js
+++ js/home.js
@@ -1,49 +1,74 @@
-
 /* Seleccionando los elementos del DOM. */
 // selectores de productos
 const cardsContainer = document.querySelector('.cards-container');
-const buttonMenu = document.querySelector('.button_menu-item');
 // selectores de menu
 const menuContent = document.querySelector('.menu-content');
+const buttonMenu = document.querySelector('.button_menu-item');
 const buttonCloseMenu = document.querySelector('.button-close');
-// // selectores de posición
-// const buttonProductos = document.querySelector('content_footer-content_items');
-// const buttonInventario = document.querySelector('.content_footer-content_inventory');
-// const buttonEstadísticas = document.querySelector('.content_footer-content_levels');
-// const divDecorativa = document.querySelector('.footer_nav-linea_decorativa');
+// selectores de posición
+// botones
+const buttonItems = document.querySelector('.content_footer-content_items');
+const buttonInventario = document.querySelector('.content_footer-content_inventory');
+const buttonEstadísticas = document.querySelector('.content_footer-content_levels');
+const presentPosition = document.querySelector('.present_position');
+// 
+
+/* Agregar un detector de eventos */
+// menu
+buttonMenu.addEventListener('click', cambio => toggleElement(menuContent,'transición'));
+buttonCloseMenu.addEventListener('click', cambio => toggleElement(menuContent,'transición'));
 
 
-/* Agregar un detector de eventos al menu*/
-buttonMenu.addEventListener('click', cambio => { toggleElement(menuContent) });
-buttonCloseMenu.addEventListener('click', cambio => { toggleElement(menuContent) });
 
-// /* Agregar un detector de eventos al selectores de posición*/
-// buttonProductos.addEventListener('click', cambio => { toggleElement(buttonEstadísticas), toggleElement(buttonInventario) });
-// buttonInventario.addEventListener('click', cambio => { toggleElement(buttonProductos), toggleElement(buttonEstadísticas) });
-// buttonEstadísticas.addEventListener('click', cambio => { toggleElement(buttonProductos), toggleElement(buttonInventario) });
+// footer
+buttonItems.addEventListener('click', selección => { positionSelectors('Items') });
+buttonInventario.addEventListener('click', selección => { positionSelectors('Inventario') });
+buttonEstadísticas.addEventListener('click', selección => { positionSelectors('Estadísticas') });
 
-/**
- * Si el elemento menuContent tiene la clase inactiva, elimínela. Si no tiene la clase inactiva,
- * agrégala.
- */
-function toggleElement(elemento) {
-    elemento.classList.toggle('inactive');
-}
+function positionSelectors(selección) {
+    Position = elem => elem.querySelector('.footer_nav-linea_decorativa');
 
-/**
- * Agrega comas a un número.
- * @param x - El número a formatear.
- * @returns el valor de las partes variables.
- */
-function numberWithCommas(x) {
-    var parts = x.toString().split(".");
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-    return parts.join(".");
-}
+    if (selección == 'Items') {
+        buttonItems.lastElementChild.classList.remove('inactive')
+        Position(buttonItems).previousElementSibling.classList.add('present_position')
 
+        buttonInventario.lastElementChild.classList.add('inactive')
+        Position(buttonInventario).previousElementSibling.classList.remove('present_position')
+
+        buttonEstadísticas.lastElementChild.classList.add('inactive')
+        Position(buttonEstadísticas).previousElementSibling.classList.remove('present_position')
+
+    } else if (selección === 'Inventario') {
+        buttonItems.lastElementChild.classList.add('inactive')
+        Position(buttonItems).previousElementSibling.classList.remove('present_position')
+
+        buttonInventario.lastElementChild.classList.remove('inactive')
+        Position(buttonInventario).previousElementSibling.classList.add('present_position')
+
+        buttonEstadísticas.lastElementChild.classList.add('inactive')
+        Position(buttonEstadísticas).previousElementSibling.classList.remove('present_position')
+
+    } else if (selección === 'Estadísticas') {
+        buttonItems.lastElementChild.classList.add('inactive')
+        Position(buttonItems).previousElementSibling.classList.remove('present_position')
+
+        buttonInventario.lastElementChild.classList.add('inactive')
+        Position(buttonInventario).previousElementSibling.classList.remove('present_position')
+
+        buttonEstadísticas.lastElementChild.classList.remove('inactive')
+        Position(buttonEstadísticas).previousElementSibling.classList.add('present_position')
+    };
+};
+
+
+function toggleElement(elemento,propiedad) {
+        elemento.classList.toggle(propiedad);
+};
+
+// area productos
 const productList = [];
 
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < 50; i++) {
     productList.push({
         name: 'Pack Botellas De Tinta X2 Epson T544',
         price: 74900,
@@ -67,11 +92,20 @@ for (var i = 0; i < 10; i++) {
 };
 
 
+
 /**
  * Toma una matriz de objetos, crea un div para cada objeto y agrega el div al DOM.
  * @param arr - una matriz de objetos
  */
 function renderProducts(arr) {
+    /**Agrega comas a un número.
+    * @param x - El número a formatear.
+    * @returns el valor de las partes variables.*/
+    function numberWithCommas(x) {
+        var parts = x.toString().split(".");
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        return parts.join(".");
+    }
     for (product of arr) {
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
